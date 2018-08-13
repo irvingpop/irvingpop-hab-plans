dd62847312a18deed2124654e02eef6cdb272cdb
Irving Popovetsky <irving@chef.io>
2018-08-13 09:13:50 -0700

    Initial proof-of-concept, credit to echohack for the inspiration
    Signed-off-by: Irving Popovetsky <irving@chef.io>

---
+++ hab_bastion/README.md
@@ -0,0 +1,9 @@
+# Habitat package: hab_bastion
+
+## Description
+
+This plan simply runs a supervisor, for purposes of providing a "permanent peer"
+
+## Usage
+
+See the `kubernetes/` folder for an example (TODO)
---
+++ hab_bastion/plan.sh
@@ -0,0 +1,16 @@
+pkg_name=hab_bastion
+pkg_origin=irvingpop
+pkg_version="0.1.0"
+pkg_maintainer="irvingpop"
+pkg_license=("Apache-2.0")
+pkg_deps=(core/busybox-static)
+pkg_upstream_url="https://github.com/irvingpop/irvingpop-hab-plans"
+
+do_build() {
+  return 0
+}
+
+do_install() {
+  return 0
+}
+

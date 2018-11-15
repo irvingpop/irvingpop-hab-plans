072bc0441892615a3f3cb84cc7924ef37e7769fd
Irving Popovetsky <irving@chef.io>
2018-11-15 11:36:46 -0800

    Remove stdout output from hab_load, to avoid it buffering in hab-launcher.  also add some tools
    Signed-off-by: Irving Popovetsky <irving@chef.io>

--- hab_load/plan.sh
+++ hab_load/plan.sh
@@ -1,9 +1,9 @@
 pkg_name=hab_load
 pkg_origin=irvingpop
-pkg_version="0.1.0"
+pkg_version="0.2.0"
 pkg_maintainer="irvingpop"
 pkg_license=("Apache-2.0")
-pkg_deps=(core/busybox-static)
+pkg_deps=(core/busybox-static irvingpop/libleak core/tcpdump core/curl)
 pkg_upstream_url="https://github.com/irvingpop/irvingpop-hab-plans"
 
 do_build() {
